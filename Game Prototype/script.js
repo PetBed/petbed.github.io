f29aade5c8b6c4796e80def6295b9e56b21f135e
PetBed <roti_canai_telur@outlook.com>
2023-11-02 01:41:32 +0800

    Balanced (A bit) of gen & added buy function

--- Game Prototype/script.js
+++ Game Prototype/script.js
@@ -1,9 +1,13 @@
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+const findObjectWithID = (objectID) => itemList.find(o => o.priority === objectID)
+const roundValue = (value, decimalPoints) => Math.floor(value * Math.pow(10, decimalPoints)) / Math.pow(10, decimalPoints)
+
 var cash = 0, buyAmount = 1;
 var itemList = [];
 
-createNewItem(1, 1, "Item 1", 1, 2, 1.2);
-createNewItem(2, 1, "Item 2", 2, 5, 1.45);
-createNewItem(3, 1, "Item 3", 3, 10, 1.7);
+createNewItem(1, 1, "Item 1", 1, 10, 1.05);
+createNewItem(2, 0, "Item 2", 2, 250, 1.20);
+createNewItem(3, 0, "Item 3", 3, 5000, 1.45);
 
 
 function createNewItem(itemID, value, buttonText, priority, basePrice, ratePrice)
@@ -14,10 +18,10 @@ function createNewItem(itemID, value, buttonText, priority, basePrice, ratePrice
     obj.buttonID = "button"+itemID;
     obj.text = buttonText;
     obj.basePrice = basePrice;
-    obj.nextPrice = 0;
     obj.ratePrice = ratePrice;
     obj.ownedItemBaseAmount = value
-    obj.ownedItemAmount = value;
+    obj.nextPrice = calculateItemPrice(obj.basePrice, obj.ratePrice, obj.ownedItemBaseAmount);
+    obj.ownedItemAmount = 0;
     obj.multipliers = 0;
     obj.baseProduction = 1; 
     obj.priority = priority;
@@ -31,13 +35,14 @@ function createNewItem(itemID, value, buttonText, priority, basePrice, ratePrice
 
     const button = document.createElement("button");
     button.setAttribute("id", obj.buttonID);
+    button.onclick = function() {buyItem(itemID)};
     const buttonDiv = document.getElementById("buttons");
     buttonDiv.appendChild(button);
 }
 
 function gameLoop()
 {
-    // console.log(itemList);
+    // console.log(roundValue(12.31283831497134, 2));
     updateValues();
     updateUI();
 }
@@ -47,11 +52,11 @@ function updateUI()
     for(i = 0; i < itemList.length; i++)
     {
         item = itemList[i];
-        document.getElementById(item.valueID).innerHTML = item.ownedItemAmount;
+        document.getElementById(item.valueID).innerHTML = item.ownedItemAmount + item.ownedItemBaseAmount;
         document.getElementById(item.buttonID).innerHTML = `<i>${item.text}</i> <b>+${buyAmount}</b> </br> ${item.nextPrice}`;
     }
 
-    document.getElementById("valueCash").innerHTML = `Cash: ${cash}`
+    document.getElementById("valueCash").innerHTML = `Cash: ${roundValue(cash, 2)}`
 }
 
 function updateValues()
@@ -60,14 +65,13 @@ function updateValues()
     for (i = 0; i < itemList.length; i++)
     {
         var obj = itemList.find(o => o.priority === curPriorityValue)
-        calculateNextPrice(obj);
         if(obj.priority > 1)
         {
-            var previousObj = itemList.find(o => o.priority === curPriorityValue - 1)
-            previousObj.ownedItemAmount += (obj.baseProduction * obj.ownedItemAmount) * (obj.multipliers + 1);
+            var previousObj = findObjectWithID(curPriorityValue - 1)
+            previousObj.ownedItemAmount += roundValue((obj.baseProduction * (obj.ownedItemAmount + obj.ownedItemBaseAmount)) * (obj.multipliers + 1), 2);
         } else if (obj.priority = 1)
         {
-            cash += (obj.baseProduction * obj.ownedItemAmount) * (obj.multipliers + 1);
+            cash += Math.max(roundValue((obj.baseProduction * (obj.ownedItemAmount + obj.ownedItemBaseAmount)) * (obj.multipliers + 1) - (obj.baseProduction/2), 2), 0);
         } else {
             console.warn(`Object id:${obj.id} has a priority of ${obj.priority}`);
         }
@@ -75,11 +79,24 @@ function updateValues()
     }
 }
 
-function calculateNextPrice(item)
+function calculateItemPrice(basePrice, ratePrice, ownedItemBaseAmount)
 {
-    item.nextPrice = item.basePrice * Math.pow(item.ratePrice, item.ownedItemBaseAmount)
-    // console.log(item.basePrice, item.ratePrice, item.ownedItemBaseAmount)
-    // console.log(item.nextPrice)
+    console.log(basePrice, ratePrice, ownedItemBaseAmount, buyAmount, basePrice * ((Math.pow(ratePrice, ownedItemBaseAmount) * (Math.pow(ratePrice, buyAmount) - 1)) / (ratePrice - 1)))
+    return roundValue((basePrice * ((Math.pow(ratePrice, ownedItemBaseAmount) * (Math.pow(ratePrice, buyAmount) - 1)) / (ratePrice - 1))), 2);
 }
+
+function buyItem(id)
+{
+    var obj = findObjectWithID(id)
+    if(cash >= obj.nextPrice)
+    {
+        cash -= obj.nextPrice;
+        obj.ownedItemBaseAmount += buyAmount;
+        obj.nextPrice = calculateItemPrice(obj.basePrice, obj.ratePrice, obj.ownedItemBaseAmount);
+    }
+    console.log("buttonClicked!", id, obj.ownedItemBaseAmount)
+    updateUI();
+}
+
 
 setInterval(gameLoop, 1000);

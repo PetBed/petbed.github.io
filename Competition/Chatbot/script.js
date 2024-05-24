18b355f9749185c005009a161132b7d9eb4dac02
PetBed <71561167+PetBed@users.noreply.github.com>
2024-05-25 01:59:36 +0800

    Updated visuals and updated script in accordance

--- Competition/Chatbot/script.js
+++ Competition/Chatbot/script.js
@@ -1,7 +1,7 @@
 const textDiv = document.getElementById("texts")
 const response = 
 [
-    "Answer 1",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam egestas convallis orci. Morbi posuere ultrices libero vel dictum. Duis vitae rhoncus orci. In sit amet turpis nec arcu venenatis luctus. Mauris auctor lacinia ante sit amet scelerisque. In quam mauris, aliquam sed dapibus sed, efficitur vel nulla. Donec nec ligula nec magna varius pulvinar eu dictum dolor. Donec mattis laoreet eros eget pulvinar. Ut at mi sodales, ornare massa eget, scelerisque mi. In magna libero, porttitor sed odio eget, ultricies fermentum risus. Cras in mollis libero. Aenean sed eros tempor, vestibulum urna semper, rutrum ante. Ut posuere aliquam elit eget placerat. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     "Answer 2",
     "Answer 3",
     "Answer 4"
@@ -9,41 +9,79 @@ const response =
 
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 var sendingMessage = false;
-// function sendMessage(elementNum) {
-//     sendAsyncMessage(elementNum);
-// }
 
 const sendMessage = async (elementNum) => {
     if (!sendingMessage) {
-        sendingMessage = true;
+        disableSelections();
         var selectionId = "selection-";
         selectionId += elementNum;
         createNewText("user-text", document.getElementById(selectionId).innerHTML);
+        scrollDown();
         await sleep(300);
         createLoadingBubble();
-        await sleep(1000);
+        scrollDown();
+        await sleep(800);
         deleteLoadingBubble();
         createNewText("ai-text", response[elementNum - 1]);
-        sendingMessage = false;
+        scrollDown();
+        enableSelections();
     }
 }
 
 
 function createNewText(senderClass, text) {
+    const newChildDiv = document.createElement("div")
+    if (senderClass == "user-text") {
+        newChildDiv.classList.add("user-profile");
+    } else {
+        newChildDiv.classList.add("ai-profile");
+    }
     const newDiv = document.createElement("div");
-    newDiv.classList.add("text-box", senderClass)
-    newDiv.innerHTML = text;
+    const newP = document.createElement("p");
+    newDiv.classList.add("text-box", senderClass);
+    newP.innerHTML = text;
+    newDiv.appendChild(newChildDiv);
+    newDiv.appendChild(newP);
     textDiv.appendChild(newDiv);
 }
 
 function createLoadingBubble() {
     const newDiv = document.createElement("div");
+    const newChildDiv = document.createElement("div")
+    newChildDiv.classList.add("ai-profile");
+    const newP = document.createElement("p");
     newDiv.id = "loading-bubble";
-    newDiv.innerHTML = "...";
+    newDiv.classList.add("text-box", "ai-text")
+    newP.innerHTML = "...";
+    newDiv.appendChild(newChildDiv);
+    newDiv.appendChild(newP);
     textDiv.appendChild(newDiv);
 }
-
 function deleteLoadingBubble() {
     const loadingBubble = document.getElementById("loading-bubble");
     loadingBubble.remove();
 } 
+
+function disableSelections() {
+    sendingMessage = true;
+    var divs = document.querySelectorAll('.selection');
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].classList.add('selection-disabled');
+    }
+}
+function enableSelections() {
+    sendingMessage = false;
+    var divs = document.querySelectorAll('.selection');
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].classList.remove('selection-disabled');
+    }
+}
+
+function scrollDown() {
+    const newDiv = document.createElement("div");
+    newDiv.id = "scroll-down";
+    textDiv.appendChild(newDiv);
+    const scrollDownDiv = document.getElementById("scroll-down");
+    scrollDownDiv.scrollIntoView();
+    scrollDownDiv.remove();
+}

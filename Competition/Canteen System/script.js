15a95a4c0743c4371e454b28423f636a0966c649
PetBed <roti_canai_telur@outlook.com>
2024-05-28 19:34:42 +0800

    Added sorting by orders, fixed bug where price sorting is wrong, added new order generator

--- Competition/Canteen System/script.js
+++ Competition/Canteen System/script.js
@@ -1,7 +1,9 @@
 const tableData = document.getElementsByClassName("table-data")[0];
+const tableHead = document.getElementsByClassName("table-head")[0];
 const sortOption = document.getElementById("sort-options");
 const reverseOption = document.getElementById("reverse");
 
+var menu = ["Food", "Drinks", "Snacks"];
 var subMenu = [
     ["Nasi Ayam", "Nasi Kari", "Kueh Tiaw", "Mi Goreng"],
     ["Milo-S", "Milo-L", "Sirap", "Sprite"],
@@ -15,6 +17,7 @@ var subMenuPrice = [
 var orderListJSON, orderList, newOrderListJSON;
 var sortedOrderList = [];
 var reverseSort = false;
+var orderStatus = [];
 
 init();
 setInterval(async () => {
@@ -41,7 +44,7 @@ async function loop() {
     console.log("Something changed!");
     orderListJSON = newOrderListJSON;
     orderList = orderListJSON.ords
-    createTable(sortOption.value);
+    await createTable(sortOption.value);
 }
 
 async function getOrders() {
@@ -50,11 +53,14 @@ async function getOrders() {
         .then(json => {
             json = JSON.stringify(json);
             json = json.replaceAll("\\", "");
+            console.log(json);
             json = json.replace('ords\":\"', "ords\":");
+            console.log(json);
             json = json.replace('}]\"}', "}]}");
+            console.log(json);
             orderListJSON = JSON.parse(json);
             // orderListJSON
-            console.log(orderListJSON); 
+            // console.log(orderListJSON); 
             return orderListJSON;
         });
 
@@ -63,43 +69,65 @@ async function getOrders() {
 
 async function init() {
     await getOrders();
-    createTable(sortOption.value);
+    await createTable(sortOption.value);
 }
 
 //==================================================
 //Create table
-function createTable(sortType) {
+async function createTable(sortType) {
     var orders = "";
     var time;
     var cost;
     tableData.innerHTML = "";
-    sortOrderList(sortType);
+    tableHead.innerHTML = "";
+    
+    if (sortType != "order") {
+        tableHead.innerHTML = `
+        <tr>
+            <th>No.</th>
+            <th>Name</th>
+            <th>Class</th>
+            <th>Time</th>
+            <th>Order</th>
+            <th>Cost</th>
+        </tr>`
 
-    for (let i = 0; i < orderList.length; i++) {
-        orders = "";
-        time = "";
-        cost = 0;
-        //set order
-        console.log(orderList[sortedOrderList[i]])
-        for (let j = 0; j < orderList[sortedOrderList[i]].o.length; j++) {
-            let menuIndex = orderList[sortedOrderList[i]].o[j][0];
-            let itemIndex = orderList[sortedOrderList[i]].o[j][1];
+        await sortOrderList(sortType);
+        for (let i = 0; i < orderList.length; i++) {
+            orders = "";
+            time = "";
+            cost = 0;
+            //set order
+            for (let j = 0; j < orderList[sortedOrderList[i]].o.length; j++) {
+                let menuIndex = orderList[sortedOrderList[i]].o[j][0];
+                let itemIndex = orderList[sortedOrderList[i]].o[j][1];
+                let labelId = "" + i + j;
 
-            orders += (j + 1) + ". " + subMenu[menuIndex][itemIndex] + "<br>";
+                orders += `<input type=checkbox id="input-${labelId}"> <label for="input-${labelId}">` + (j + 1) + ". " + subMenu[menuIndex][itemIndex] + "</label><br>";
+            }
+            
+            //set cost
+            for (let j = 0; j < orderList[sortedOrderList[i]].o.length; j++) {
+                let menuIndex = orderList[sortedOrderList[i]].o[j][0];
+                let itemIndex = orderList[sortedOrderList[i]].o[j][1];
+    
+                cost += parseInt(subMenuPrice[menuIndex][itemIndex]);
+            }
+            cost = "RM" + cost;
+    
+            //set time
+            time += orderList[sortedOrderList[i]].t.charAt(0) + orderList[sortedOrderList[i]].t.charAt(1) + ":" + orderList[sortedOrderList[i]].t.charAt(2) + orderList[sortedOrderList[i]].t.charAt(3);
+            createTableRow(orders, orderList[sortedOrderList[i]].n, orderList[sortedOrderList[i]].c, cost, time);
         }
-        
-        //set cost
-        for (let j = 0; j < orderList[sortedOrderList[i]].o.length; j++) {
-            let menuIndex = orderList[sortedOrderList[i]].o[j][0];
-            let itemIndex = orderList[sortedOrderList[i]].o[j][1];
-
-            cost += parseInt(subMenuPrice[menuIndex][itemIndex]);
-        }
-        cost = "RM" + cost;
-
-        //set time
-        time += orderList[sortedOrderList[i]].t.charAt(0) + orderList[sortedOrderList[i]].t.charAt(1) + ":" + orderList[sortedOrderList[i]].t.charAt(2) + orderList[sortedOrderList[i]].t.charAt(3);
-        createTableRow(orders, orderList[sortedOrderList[i]].n, orderList[sortedOrderList[i]].c, cost, time);
+    } else {
+        tableHead.innerHTML = `
+        <tr>
+            <th>No.</th>
+            <th>Order Type</th>
+            <th>Count</th>
+            <th>Done</th>
+        </tr>`
+        writeOrders();
     }
 }
 
@@ -111,11 +139,11 @@ function createTableRow(order, name, className, cost, time) { //used className i
     newRow.innerHTML = 
     `
         <td>${tableData.childElementCount+1}.</td>
-        <td>${order}</td>
         <td>${name}</td>
         <td>${className}</td>
-        <td>${cost}</td>
         <td>${time}</td>
+        <td>${order}</td>
+        <td>${cost}</td>
     `;
     tableData.appendChild(newRow);
 
@@ -140,9 +168,18 @@ function sortOrderList(sortType) {
 
                 cost += parseInt(subMenuPrice[menuIndex][itemIndex]);
             }
+            if (cost < 10) {
+                cost = "00" + cost;
+            } else if (cost < 100) {
+                cost = "0" + cost;
+            }
             tempArray.push(cost);
         }
         
+    } else if (sortType == "name") {
+        for (let i = 0; i < orderList.length; i++) {
+            tempArray.push(orderList[i].n)
+        }
     } else if (sortType == "class") {
         var teacher = [];
         var pa = [];
@@ -208,6 +245,8 @@ function sortOrderList(sortType) {
     
     if (sortType != "class") {
         sortedTempArray = tempArray.toSorted();
+        console.log(tempArray)
+        console.log(sortedTempArray)
     }
 
     if (reverseSort) {
@@ -222,7 +261,7 @@ function sortOrderList(sortType) {
         // console.log(tempArray)
         if (sortedTempArray[i] == sortedTempArray[i + 1]) {
             for (let j = 0; j < tempArray.length - i; j++) {
-                console.log(sortedTempArray);
+                // console.log(sortedTempArray);
                 if (sortedTempArray[i].toString() == sortedTempArray[i + j]) {
                     dupeCount++;
                 } else {
@@ -244,6 +283,53 @@ function sortOrderList(sortType) {
         }
 
         // console.log(sortedOrderList);
+    }
+}
+
+function writeOrders() {
+    orderStatus = [];
+    // console.log(orderList);
+
+    for (let i = 0; i < orderList.length; i++) {
+        for (let j = 0; j < orderList[i].o.length; j++) {
+            // console.log(subMenu[orderList[i].o[j][0]][orderList[i].o[j][1]]);
+
+            const k = orderStatus.findIndex(e => e.name === subMenu[orderList[i].o[j][0]][orderList[i].o[j][1]]);
+            if (k > -1) {
+                orderStatus[k].count += 1;  
+            } else {
+                var newObject = {}
+                newObject.name = subMenu[orderList[i].o[j][0]][orderList[i].o[j][1]];
+                newObject.count = 1;
+                orderStatus.push(newObject);
+            }
+        }
+    }
+
+    for (let i = 0; i < 3; i++) {
+        var sortedOrderStatus = [];
+        const newRowHeader = document.createElement("tr");
+        newRowHeader.classList.add("table-order-header");
+        newRowHeader.innerHTML = `<td colspan=4>${menu[i]}</td>`;
+        tableData.appendChild(newRowHeader);
+
+        for (let j = 0; j < orderStatus.length; j++) {
+            if (subMenu[i].indexOf(orderStatus[j].name) > -1) {
+                sortedOrderStatus.push(orderStatus[j]);
+            }
+        }
+    
+        for (let j = 0; j < sortedOrderStatus.length; j++) {
+            const newRow = document.createElement("tr");
+            newRow.id = `table-row-${sortedOrderStatus[j].name.toLowerCase()}`;
+            newRow.innerHTML = 
+            `
+            <td>${j+1}.</td>
+            <td>${sortedOrderStatus[j].name}</td>
+            <td>${sortedOrderStatus[j].count}</td>
+            `;
+            tableData.appendChild(newRow);
+        }
     }
 }
 
@@ -283,3 +369,4 @@ function getCookie(cname) {
     }
     return "";
 }
+

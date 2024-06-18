778f3da292b31341a3408fc088238c6c3a4520a8
PetBed <roti_canai_telur@outlook.com>
2024-06-18 13:36:55 +0800

    v0.0.2

--- Games/Paper Legacy/script.js
+++ Games/Paper Legacy/script.js
@@ -1,11 +1,13 @@
 var data = {
   boxes: {
     credit: {
-      max: 10,
+      size: 10,
+      sizeCostBase: 100,
+      sizeCostGrowth: 1.07,
       contents: [],
       paperRange: 10,
-      cooldownDraw: 1000,
-      cooldownRefill: 5000,
+      cooldownDraw: 30,
+      cooldownRefill: 500,
     }
   },
   currencies: {
@@ -20,7 +22,7 @@ var boxStatus = {
   }
 }
 
-var unshuffledNumbers = shuffledNumbers = [];
+var unshuffledNumbers = shuffledNumbers = numberRange = [];
 var refreshRate = 30;
 
 for (let i = 0; i < 100 ; i++) {
@@ -29,16 +31,23 @@ for (let i = 0; i < 100 ; i++) {
 
 function refillBox(boxType) {
   data.boxes[boxType].contents.length = 0;
+  shuffledNumbers = [];
   
-  shuffledNumbers = [ ...unshuffledNumbers ]
-  shuffledNumbers.splice(data.boxes[boxType].paperRange, shuffledNumbers.length);
+  numberRange = [ ...unshuffledNumbers ]
+  numberRange.splice(data.boxes[boxType].paperRange, numberRange.length);
+  console.log(numberRange)
+
+  for (i = 0; i < Math.ceil(data.boxes["credit"].size / data.boxes["credit"].paperRange); i++) {
+    shuffledNumbers.push( ...numberRange );
+  }
 
   for (let i = shuffledNumbers.length - 1; i > 0; i--) {
     const u = Math.floor(Math.random() * (i + 1));
 		[shuffledNumbers[i], shuffledNumbers[u]] = [shuffledNumbers[u], shuffledNumbers[i]];
 	}
 
-  data.boxes[boxType].contents = [ ...shuffledNumbers.slice(0, data.boxes[boxType].max) ];
+  data.boxes[boxType].contents = [ ...shuffledNumbers.slice(0, data.boxes[boxType].size) ];
+  console.log(data.boxes["credit"].contents)
 }
 refillBox("credit");
 
@@ -57,8 +66,18 @@ document.getElementById("draw-paper-credit").addEventListener("click", () => {
 });
 
 document.getElementById("upgrade-range-credit").addEventListener("click", () => {
-	
+	if (data.currencies.credit >= (data.boxes["credit"].paperRange - 9) * 50) {
+    data.currencies.credit -= (data.boxes["credit"].paperRange - 9) * 50;
+    data.boxes["credit"].paperRange += 1;
+  }
 });
+
+document.getElementById("upgrade-size-credit").addEventListener("click", () => {
+  if (data.currencies.credit >= data.boxes["credit"].sizeCostBase * Math.pow(data.boxes["credit"].sizeCostGrowth, data.boxes["credit"].size - 10)) {
+    data.currencies.credit -= (data.boxes["credit"].sizeCostBase * Math.pow(data.boxes["credit"].sizeCostGrowth, data.boxes["credit"].size - 10)).toFixed(2);
+    data.boxes["credit"].size += 1;
+  }
+})
 
 
 function updateCooldowns() {
@@ -70,10 +89,15 @@ function updateCooldowns() {
 }
 
 function draw() {
+  document.getElementById("credit-amount").innerHTML = `Credits: ${data.currencies["credit"].toFixed(2)}`;
   document.getElementById("paper-left-credit").innerHTML = `Papers Left: ${data.boxes["credit"].contents.length}`;
-  document.getElementById("credit-amount").innerHTML = `Credits: ${data.currencies["credit"]}`;
   document.getElementById("last-paper-drawn-credit").innerHTML = `Paper Drawn: ${boxStatus["credit"].lastPaperDrawn}`;
   document.getElementById("cooldown-time-credit").innerHTML = `Refill Cooldown: ${(boxStatus["credit"].cooldownRefill / 1000).toFixed(2)}s`;
+  document.getElementById("box-size-credit").innerHTML = `Box Size: ${data.boxes["credit"].size}`;
+  document.getElementById("paper-range-credit").innerHTML = `Paper Range: 1 - ${(data.boxes["credit"].paperRange)}`;
+
+  document.getElementById("upgrade-range-credit").innerHTML = `Upgrade Paper Range (${(data.boxes["credit"].paperRange - 9) * 50} Credits)`;
+  document.getElementById("upgrade-size-credit").innerHTML = `Upgrade Box Size (${(data.boxes["credit"].sizeCostBase * Math.pow(data.boxes["credit"].sizeCostGrowth, data.boxes["credit"].size - 10)).toFixed(2)} Credits)`;
 }
 
 var gameLoop = window.setInterval(() => {
